7634a1b7f83d0b96682b6a2a1d9445f8ced6e205
danny-englander <dannyenglander311@gmail.com>
2022-04-25 14:53:56 -0400

    Minor updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,20 @@
 const yaml = require("js-yaml");
 
 module.exports = function (eleventyConfig) {
-
   // This passes through anything in the assets folder into the build, i.e. _site
   eleventyConfig.addPassthroughCopy("src/assets/")
+
   // Watch for css changes.
   eleventyConfig.addWatchTarget("src/assets/css/")
   eleventyConfig.addWatchTarget("_data")
 
+  // Register yaml data.
   eleventyConfig.addDataExtension("yml", contents => yaml.load(contents));
-  // eleventyConfig.addDataExtension("json", contents => json.load(contents));
+
+  // Limit filter.
+  eleventyConfig.addFilter("limit", function (array, limit) {
+    return array.slice(0, limit);
+  });
 
   return {
     dir: {
